580bb2988f1dc1a4b14a10130c7a2f3add7560f4
James Liu <jamesliu@au1.ibm.com>
2019-02-25 12:33:12 +1100

    feat: add get_top_flavor method, add FlavorProfile type, fix docstring formatting

--- preprocessing/ingredient.py
+++ preprocessing/ingredient.py
@@ -1,16 +1,18 @@
+"""Defines the Ingredient class to store processed information on an ingredient.
+"""
+
 from typing import Dict, List
 import math
 
-# FlavorProfile is an ordered array of flavors and the count of their
+FlavorProfile = Dict[str, any]
+# FlavorProfiles is an ordered array of flavors and the count of their
 # occurrences, ordered from the highest to lowest occurring flavor.
 # [{"bitter": 5, "sweet: 1"}]
-FlavorProfiles = List[Dict[str, any]]
+FlavorProfiles = List[FlavorProfile]
 
-"""
-An ingredient extracted from the FlavorDB database.
-"""
 class Ingredient:
-
+    """An ingredient extracted from the FlavorDB database.
+    """
     def __init__(self, name: str, category: str, flavor_db_entity_id: int,
                  flavor_profiles: FlavorProfiles):
         self.name = name
@@ -21,11 +23,15 @@ class Ingredient:
     def __str__(self):
         return f"{self.name} - {self.category} : {self.id}"
 
-    """
-    Returns the top pct of flavors from the flavor_profile. If no pct is
-    supplied, defaults to 25%. The ceiling is used when the cutoff index
-    is computed.
-    """
     def get_top_flavors(self, pct: int = 25) -> FlavorProfiles:
+        """ Returns the top pct of flavors from the flavor_profiles. If no pct is
+        supplied, defaults to 25%. The ceiling is used when the cutoff index
+        is computed.
+        """
         cutoff_idx = math.ceil(len(self.flavor_profiles) * (pct/100))
         return self.flavor_profiles[:cutoff_idx]
+
+    def get_top_flavor(self) -> FlavorProfile:
+        """Returns the top flavor from the flavor_profiles array.
+        """
+        return self.flavor_profiles[0]
