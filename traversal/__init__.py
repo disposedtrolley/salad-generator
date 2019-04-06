e372a6a00edfc78c19427e15a5e34adad3956423
James Liu <james_liu@fastmail.com>
2019-04-06 17:04:37 +1100

    feat: add Traverser class stub

---
+++ traversal/__init__.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("..")
+from models.ingredient import Ingredient, IngredientType
+from models.graph import Graph
+from preprocessing.main import create_mappings
+from traverser import Traverser
+
+if __name__ == "__main__":
+    mappings = create_mappings("../data")
+    g = Graph(mappings)
+    t = Traverser(g)
---
+++ traversal/traverser.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.append("..")
+from typing import Dict, List, Tuple
+from models.ingredient import IngredientType
+from models.graph import Graph
+
+class Traverser:
+    """
+    Provides methods to traverse an ingredient graph generated
+    during preprocessing. A traversal through the graph means
+    starting at some ingredient and recursively presenting the
+    next best candidates according to an aggregate strength
+    calculation.
+    """
+
+    # Define the limits around a valid salad composition.
+    # Traversal can optionally conclude after the minimums
+    # are reached, and must terminate once the maximums are
+    # reached.
+    SALAD_COMPOSITION_LIMITS = {
+        IngredientType.BASE: (1, 2),
+        IngredientType.TOPPING: (3, 6),
+        IngredientType.PROTEIN: (1, 2),
+        IngredientType.DRESSING: (1, 1)
+    }
+
+    def __init__(self, graph: Graph):
+        self.graph = graph
+        self.ingredients = graph.get_nodes()
+        self.salad_composition = []
+        self.start_traversal()
+
+    def start_traversal(self):
+        """
+        Begins the ingredient graph traversal process.
+        """
+        assert self.graph is not None
+        assert self.ingredients is not None
+        assert len(self.ingredients) >= 1
+        self._print_ingredient_choices(self._get_remaining_candidates())
+
+    def _get_remaining_candidates(self):
+        """
+        Returns a list of strongest remaining ingredient candidates
+        based on:
+
+        1) The state of the current salad composition as compared
+           to the limits.
+        2) The aggregate strength of the shared molecules of
+           previously selected ingredients to unselected candidates.
+        """
+        return self.ingredients
+
+    def _get_number_selected(self, ingredient_type):
+        """
+        Returns the number of ingredients in the current salad
+        composition of the given ingredient_type.
+        """
+        filtered_list = list(filter(lambda ig: ig.type == ingredient_type, self.salad_composition))
+        return len(filtered_list)
+
+
+    def _print_ingredient_choices(self, remaining_choices):
+        """
+        Displays a list of ingredient choices for the user
+        to select. The index of the ingredient is the choice
+        key value.
+        """
+        for (idx, ingredient) in enumerate(remaining_choices):
+            print(f"{idx}. {ingredient.name}")
